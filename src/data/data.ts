ef8f5251f5f99be928f9b21ff3e1027c76161ec1
Benedict <jokanolaadedipupo@gmail.com>
2021-11-05 18:00:48 +0100

    price-route

--- src/data/data.ts
+++
@@ -1,6 +0,0 @@
-export const locationRoute = [
-["ikeja","maryland","ilupeju","palmgroove","jibowu","yaba","surulere"],
-["oshodi","isolo","akoka"],
-["amuwo","festac","satelite"],
-["apapa","trade-fair","ojo-alaba"]
-];
---
+++ src/data/location.ts
@@ -0,0 +1,12 @@
+export const locationRoute = [
+["ikeja","maryland","ilupeju","palmgroove","jibowu","yaba","surulere"],
+["oshodi","isolo","akoka"],
+["amuwo","festac","satelite"],
+["apapa","trade fair","ojo alaba"],
+["ogudu","ketu","magodo","omole","ojudu","ogba","agege"],
+["IJU", "ISHAGA","ABULE EGBA","ALIMOSHO","EJIGBO","IKOTUN","IGANDO"],
+["MARINA","LAGOS ISLAND","IKOYI","V I"],
+["IKOTA","VGC","AJAH","ABRAHAM ADESANYA","BADORE","SANGOTEDO","AWOYAYA"],
+["IKORODU"],
+["ELEKO","EPE","AGBARA","BADAGRY","DANGOTE REFINARY" ,"REDEEM CAMP","MOWE","IBAFO"]
+];
